a110cb5cac2ecf5b16179fa644cf1cddb571c6cf
Ivan Novyshev <I-N-Zone@yandex.ru>
2021-06-01 01:18:47 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RoboCommerc
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public List<Tuple<string, IEnumerable<string>>> testCollection;
+        private void FindButton_Click(object sender, RoutedEventArgs e)
+        {
+            string[] assemblies = Directory.GetFiles($"{PathLibraryTextBox.Text}", "*.dll");
+            testCollection = new List<Tuple<string, IEnumerable<string>>>();
+            Parallel.ForEach(assemblies, assemblyPath =>
+            {
+                var loadAssembly = Assembly.LoadFile(assemblyPath);
+                var classesFromAssembly = loadAssembly.GetTypes().Where(x => x.IsClass);
+                foreach (var classType in classesFromAssembly)
+                {
+                    testCollection.Add(Tuple.Create(classType.Name, classType.GetMethods().Where(x => x.IsPublic || x.IsFamily).Select(x => x.Name)));
+                }
+            });
+            ItemsControl.ItemsSource = testCollection;
+        }
+    }
+}
